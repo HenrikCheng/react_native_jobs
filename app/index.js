6fc68be283b9ce78f3c3f7f6ca090fedcade366b
HenrikCheng <henrikcheng@live.se>
2023-06-27 13:55:08 +0200

    added missing files from startup code

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text className="bg-red-100">Home or Away</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColors: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColors: COLORS.lightWhrite },
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
